4f846a49f313f750a50567fbe48c23119ffbd1b9
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-04-02 16:14:34 -0500

    f this

--- mergesort.js
+++ mergesort.js
@@ -1,7 +1,8 @@
+/* eslint-disable complexity */
 function split(wholeArray) {
   let splitNum = Math.ceil(wholeArray.length / 2);
-  let firstHalf = wholeArray.splice(0, splitNum);
-  let secondHalf = wholeArray;
+  let firstHalf = wholeArray.slice(0, splitNum);
+  let secondHalf = wholeArray.slice(splitNum);
 
   return [firstHalf, secondHalf];
 }
@@ -10,15 +11,18 @@ function merge(arrayOfArrays) {
   let mergedArray = [];
   let firstArray = arrayOfArrays[0];
   let secondArray = arrayOfArrays[1];
-  let totalItems = firstArray.length + secondArray.length;
-
-  for (let i = 0; i < totalItems; i++) {
-    if (!firstArray[0] || firstArray[0] > secondArray[0]) {
-      mergedArray.push(secondArray.shift());
-    } else {
-      mergedArray.push(firstArray.shift());
+  let totalItems = 1
+  if (firstArray && firstArray.length && secondArray && secondArray.length){
+    totalItems = firstArray.length + secondArray.length;
+    for (let i = 0; i < totalItems; i++) {
+      if (firstArray[0] > secondArray[0]) {
+        mergedArray.push(secondArray.shift());
+      } else {
+        mergedArray.push(firstArray.shift());
+      }
     }
-  }
+  } else { mergedArray = [...mergedArray, ...firstArray] }
+
   return mergedArray;
 }
 
@@ -26,23 +30,43 @@ function mergeSort(array) {
   let initLength = array.length;
   let splitArr = array;
   let mergedArr = [];
-  console.log(splitArr)
+
 
   let workArr = splitArr;
   splitArr = [];
+  let [first, second] = split(workArr);
+  splitArr.push(first);
+  splitArr.push(second);
 
-  console.log('firstwork',workArr)
-  for (let i = 0; i < workArr.length; i++){
-    console.log('workArr',workArr)
-    let [first, second] = split(workArr);
-    splitArr.push(first);
-    splitArr.push(second);
+  while (splitArr.length < initLength - 1){
+    workArr = splitArr;
+    splitArr = [];
+
+    for (let i = 0; i < workArr.length; i++){
+      let [first, second] = split(workArr[i]);
+
+
+      splitArr.push(first);
+
+      console.log(first, second);
+      if (second) { splitArr.push(second); }
+
+    }
+    //console.log('splitArr', splitArr);
+
   }
+  mergedArr = splitArr
 
-  // while (splitArr.length < initLength){
-  //   let [first,second] = split(array)
-  //   splitArr.push()
-  // }
 
-  console.log(splitArr);
+  while (mergedArr.length > 1) {
+
+    workArr = mergedArr
+    mergedArr = []
+    for (let i = 0; i < workArr.length; i = i + 2){
+
+      let currentMerge = [workArr[i], workArr[i + 1]]
+      mergedArr.push(merge(currentMerge))
+    }
+  }
+  return mergedArr
 }
