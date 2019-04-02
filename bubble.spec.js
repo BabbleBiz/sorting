94448c5119984d741332b8dda4de96c2afbfffc9
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-04-02 11:37:45 -0500

    all tests passing GREAT'

--- bubble.spec.js
+++ bubble.spec.js
@@ -1,13 +1,20 @@
-describe('Bubble Sort', function(){
-    it('handles an empty array', function(){
-      expect( bubbleSort([]) ).toEqual( [] );
-    });
-  });
 
+describe('Bubble Sort', function() {
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it('Sorts and array', function() {
+    expect(bubbleSort([4, 2, 10, 9])).toEqual([2, 4, 9, 10]);
+  });
+});
+
+describe('Swap', function () {
   beforeAll(function () {
-    spyOn(swap).and.callThrough();
+    spyOn(window, 'swap').and.callThrough();
   });
   it('swaps numbers the correct amount of times', function () {
-    bubbleSort([2,4,8,3,7,5,6,7]);
-    expect(swap.calls.count()).toEqual(8);
+    bubbleSort([2,4,8,6]);
+    expect(window.swap.calls.count()).toEqual(6);
   });
+})
+
